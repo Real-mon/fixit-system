8c957cff5217998c61f38c69e02005b67d3469bc
Real-mon <jusmonreal@gmail.com>
2025-09-29 23:00:03 +0800

    Setting Up Django

--- fixit_project/fixit_project/asgi.py
+++ fixit_project/fixit_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FixIT project.
+ASGI config for fixit_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FixIT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fixit_project.settings')
 
 application = get_asgi_application()
--- fixit_project/fixit_project/wsgi.py
+++ fixit_project/fixit_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FixIT project.
+WSGI config for fixit_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FixIT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fixit_project.settings')
 
 application = get_wsgi_application()
